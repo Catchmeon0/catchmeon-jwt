460d83883b5e93e4949207aa058d475ade1c18d4
Mohammed Arbaoui <arb.med241@gmail.com>
2021-01-22 18:27:38 +0100

    update key

--- src/main/java/com/catchmeon/catchmeonjwt/models/request/TweetRequest.java
+++ src/main/java/com/catchmeon/catchmeonjwt/models/request/TweetRequest.java
@@ -123,7 +123,7 @@ public class TweetRequest {
         DefaultHttpClient client = new DefaultHttpClient();
         HttpGet get = new HttpGet(url);
         get.setHeader("Content-Type", "application/json");
-        get.setHeader("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAABhkLwEAAAAATX2kSV9bdfUDzPOSx9SaqwR8l78%3DuyiHlz9DclvL2VRhKgMaW1n8xdpGeUXpoe5QHdff4ckPC0cdDT");
+        get.setHeader("Authorization", authKey);
 
 //this is response:
         HttpResponse response = client.execute(get);
